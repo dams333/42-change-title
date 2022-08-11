7b6422868c10589874078e2e70d06b010a6eb3d2
dhubleur <dhubleur@student.42.fr>
2022-08-11 22:11:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Create a `.env` replacing the `ID` and `SECRET` by your 42's crendentials and the `SESSION_COOKIE` by the cookie `_intra_42_session_production` that you have on your intranet when you are log
+
+You can run with
+```bash
+npm run test
+```
---
+++ src/index.ts
@@ -0,0 +1,68 @@
+import "dotenv/config";
+import { Client } from "42.js";
+// @ts-ignore
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const axios = require('axios').default;
+
+
+(async () => {
+    const client = new Client(
+        <string>process.env.ID,
+        <string>process.env.SECRET
+    );
+
+    const rl = readline.createInterface({ input, output });
+
+    const login = await rl.question('Login: ');
+
+    const user = await client.users.get(login);
+    if(!user)
+    {
+        console.log('User not found');
+        return;
+    }
+    //Get titles
+    const titles_user = await client.fetch("/users/" + user.id + "/titles_users?");
+
+    const titles = await client.fetch("users/" + user.id + "/titles?");
+
+    for(const idx in titles_user)
+    {
+        const text = (<any>titles[idx]).name.replace("%login", login);
+        console.log(`${idx}: ${text}`);
+    }
+
+    const tit = await rl.question('Title: ');
+
+    const title = parseInt(tit);
+    if(isNaN(title))
+    {
+        console.log('Please choose a real number');
+        return;
+    }
+
+    if(title < 0 || title >= titles_user.length)
+    {
+        console.log('Please choose a real number');
+        return;
+    }
+
+    const titleId = (<any>titles_user[title]).id;
+
+    console.log(`Put the title with id ${titleId}`);
+
+    const cookie = "_intra_42_session_production=" + process.env.SESSION_COOKIE;
+    const res = await axios.get(`https://profile.intra.42.fr/titles_users/${titleId}/selected`, { headers: { "cookie": cookie } });
+    
+    if(res.status === 200)
+    {
+        console.log('Title put');
+    }
+    else
+    {
+        console.log('Error');
+    }
+
+    rl.close();
+})();
